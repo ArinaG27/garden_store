ab53f6d0d5c46dc48c976e432ff2e24a8bd37815
ArinaG27 <arina.gav.27@gmail.com>
2023-01-22 23:05:16 +0100

    cart changed

--- src/components/CartCard/index.jsx
+++ src/components/CartCard/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import s from './index.module.css'
+import line from '../../media/line.png'
 import { cartIncrement, cartDecrement } from '../../store/reducers/cart'
 import { useDispatch } from 'react-redux'
 
@@ -13,21 +14,29 @@ export default function CartCard({ id, title, price, image, count, discont_price
 
   console.log(count);
   return (
-    <div className={s.cart_card}>
+    <div>  
+      <img src={line} alt="line" className={s.line} />
+    <div className={s.cart_card}>  
       <div>
         <img src={src_img} alt={title} />
       </div>
+
       <div>
-      <p>{ title }</p>      
-      <div className={s.triggers}>
-        <button onClick={increment}>+</button>
-        <p> { count } </p>
-        <button onClick={decrement}>-</button>
+        <p className={s.card_title}>{ title }</p> 
+
+        <div className={s.triggers}>
+          <button onClick={decrement}>-</button>
+          <p> { count } </p>
+          <button onClick={increment}>+</button>
+        </div>
+
       </div>
-      </div>
-      <div>
-        <p> {price * count}</p>
-        <p> {discont_price * count}</p>
+        <p className={s.price}> {(price * count).toFixed(2)} $ </p>
+        <div>
+        <p className={s.cross}>x</p>
+        <p className={s.discont_price}> {(discont_price * count).toFixed(2)} $</p>
+        </div>
+      
       </div>
     </div>
   )
--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -11,40 +11,57 @@ export default function CartPage() {
   console.log(cart);
 
   return (
-    <div>
-      <p> Card </p>
-      <p>Homepage/ Card  </p>
-      <p> вернуться в магазин </p>
-    <div className={s.card_section}>
-    {
+    <div className={s.card_page}> 
+
+      <p className={s.card}> Card </p>
+
+      <div className={s.card_start} >
+        <p> <a href="/">Homepage</a>/Card  </p>
+        <p> <a href="/category">Return to the shop </a> </p>
+      </div> 
+
+      <div className={s.card_section}>
+      {
         cart.length === 0 
-        ? <div className={s.empty}>The cart is empty</div>
+        ? <div className={s.empty}>The cart is empty <br /> <br /> Total: 0.00 $</div>
         : <div className={s.cart_container}>
               {
                 cart.map(el => <CartCard key={el.id} {...el} />)
               }
-          </div>
+
+          <div className={s.total_container}>
+
+              <p className={s.order}> Order details </p>  
+
+              <div className={s.button_block}>
+
+                <p className={s.total}>Total:</p> 
+                
+                <p className={s.sum}>
+                  {
+                    cart.reduce((prev, {price, count}) => prev + price * count , 0).toFixed(2)
+                  } $
+                </p>
+
+              </div >
+
+              <form className={s.form} >
+                  <input type='text' name='phone' placeholder=' your phone number ' className={s.order_form}/> <br />
+                  <button  className={s.order_button}> Order </button>
+              </form>
+
+            </div>
+
+            <div>
+              <button onClick={() => dispatch(clearCart())} className={s.clear}>Clear the cart</button>
+            </div>
+            </div>
       }
-
-      <div>      
-        <div className={s.button_block}>
-        <p>
-          <span>Total:</span>
-          {
-            cart.reduce((prev, {price, count}) => prev + price * count , 0)
-          }
-        </p>
-
       </div>
-        <form >
-            <input type='text' name='phone' placeholder=' ваш номер телефона' className={s.discount_form}/> <br />
-            <br />
-            <button  className={s.discount_button}> Заказать </button>
-        </form>
-      </div>
-       
+
+
+    
     </div>
-    <div><button onClick={() => dispatch(clearCart())}>Clear the cart</button></div>
-    </div>
+
   )
 }
